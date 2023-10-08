c60e9140b76719b9b82134d437cd329d023273f9
jorge1armando1290@gmail.com <jorge1armando1290@gmail.com>
2023-10-08 13:57:05 -0600

    fixed

--- Practicev1/src/practice/listeners/EventHandler.php
+++ Practicev1/src/practice/listeners/EventHandler.php
@@ -2,10 +2,20 @@
 
 namespace practice\listeners;
 
+use practice\kits\Spawn;
+
 use pocketmine\event\Event;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class EventHandler implements Listener {
   
+  /**
+   * @priority LOW
+   **/
+   public function onJoin(PlayerJoinEvent $ev) : void {
+     $p = $ev->getPlayer();
+     Spawn::select($p);
+   }
 }
 ?>
